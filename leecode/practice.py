23e037c413b3634eacf1f66ffb0152eb4303fc59
masterzz <545724476@qq.com>
2023-09-18 14:41:16 +0800

    增加leecode练习

---
+++ leecode/practice.py
@@ -0,0 +1,12 @@
+# 1、合并两个有序数组
+from typing import List
+
+
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        nums1 = [1.24]
+
+
