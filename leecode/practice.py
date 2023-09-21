f701e7ab1291a27e6a1efcba7361397a40f5179a
masterzz <545724476@qq.com>
2023-09-21 10:46:20 +0800

    机器学习文件补充

--- leecode/practice.py
+++ leecode/practice.py
@@ -1,12 +1,30 @@
 # 1、合并两个有序数组
 from typing import List
 
-
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
         """
         Do not return anything, modify nums1 in-place instead.
+        1、全部元素先加进来
+        2、单个非递减顺序排列，用冒泡
+        3、两个非递减排序
         """
-        nums1 = [1.24]
+        temp = []
+        for i in nums1:
+            # if cursor is None or cursor <=i :
+            temp.append(i)
+        for j in nums2:
+            temp.append(j)
+        l = len(temp)
+        for i in range(l):
+            for j in range(l):
+                pass
+        print(temp)
+
+if __name__ == '__main__':
+    n1 = [1,2,3,0,0,0]
+    n2 = [2,5,6]
+    a = Solution()
+    a.merge(nums1=n1,m=1,nums2=n2,n=2)
 
 
